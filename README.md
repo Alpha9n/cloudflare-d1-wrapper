badb532165cd198746fa54079b3e59a98e9878f4
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-06-12 00:37:58 +0900

    feature: add create, get, query database function.

--- README.md
+++ README.md
@@ -17,8 +17,8 @@ import wrapper from 'cloudflare-d1-wrapper';
 
 ## Features
 - [ ] [Get database list](https://developers.cloudflare.com/api/operations/cloudflare-d1-list-databases)
-- [ ] [Create database](https://developers.cloudflare.com/api/operations/cloudflare-d1-create-database)
+- [x] [Create database](https://developers.cloudflare.com/api/operations/cloudflare-d1-create-database)
 - [ ] [Delete database](https://developers.cloudflare.com/api/operations/cloudflare-d1-delete-database)
-- [ ] [Get database](https://developers.cloudflare.com/api/operations/cloudflare-d1-get-database)
-- [ ] [Query database](https://developers.cloudflare.com/api/operations/cloudflare-d1-query-database)
+- [x] [Get database](https://developers.cloudflare.com/api/operations/cloudflare-d1-get-database)
+- [x] [Query database](https://developers.cloudflare.com/api/operations/cloudflare-d1-query-database)
 - [ ] [Raw database query](https://developers.cloudflare.com/api/operations/cloudflare-d1-raw-database-query)
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,66 @@
-export default function d1Wrapper() {
-    
-    return ;
+import { BASE_URL, CreateDBResponse, GetDBResponse, QueryResponse } from "./consts";
+
+export default class d1Wrapper {
+    private key;
+    private headers: HeadersInit;
+
+    constructor(apiKey: string) {
+        this.key = apiKey;
+        this.headers = [
+            [ 'Content-Type', 'application/json' ],
+            [ 'Authorization', 'Bearer ' + this.key ]
+        ];
+    }
+
+    async createDatabase(
+        account_id: string,
+        database_name: string
+    ): Promise<CreateDBResponse> {
+        const res = await fetch(
+            `${BASE_URL}/accounts/${account_id}/d1/database`,
+            {
+                method: 'POST',
+                headers: this.headers,
+                body: JSON.stringify({ name: database_name })
+            }
+        );
+
+        return res.json();
+    }
+
+    async getDatabase(
+        account_id: string, 
+        database_id: string
+    ): Promise<GetDBResponse> {
+        const res = await fetch(
+            `${BASE_URL}/accounts/${account_id}/d1/database/${database_id}`,
+            { 
+                method: 'GET',
+                headers: this.headers 
+            }
+        );
+
+        return res.json();
+    }
+
+    async getQueryResponse<T>(
+        account_id: string, 
+        database_id: string, 
+        query: string, 
+        params?: string[]
+    ): Promise<QueryResponse<T>> {
+        const res = await fetch(
+            `${BASE_URL}/accounts/${account_id}/d1/database/${database_id}/query`,
+            {
+                method: 'POST',
+                headers: this.headers,
+                body: JSON.stringify({
+                    params: params ?? [],
+                    sql: query
+                })
+            }
+        );
+
+        return res.json();
+    }
 }
